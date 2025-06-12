0057755a1b94b229204f3886fc120dea60f3b1f9
piercetopp <124905970+piercetopp@users.noreply.github.com>
2025-06-11 20:27:02 -0500

    build: add flat eslint config and script

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  react.configs.recommended,
+  tsPlugin.configs['flat/recommended'],
+  prettier.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react,
+      prettier,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
